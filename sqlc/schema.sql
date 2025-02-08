1447b3a0fc86e2d37455039185a5fe4141fa98a8
austingw <senortinman@gmail.com>
2025-02-08 14:52:02 -0500

    Add basic sqlc setup/schema

---
+++ sqlc/schema.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS projects (
+  id   INTEGER PRIMARY KEY,
+  name text    NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+);
+
+CREATE TABLE IF NOT EXISTS requests (
+  id         INTEGER PRIMARY KEY,
+  project_id INTEGER NOT NULL,
+  name       TEXT    NOT NULL,
+  method     TEXT    NOT NULL,
+  url        TEXT    NOT NULL,
+  headers    TEXT,
+  body       TEXT,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (project_id) REFERENCES projects(id)
+);
