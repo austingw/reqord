d0032d0ce35f15d11851ba3ad357db33a081842e
austingw <senortinman@gmail.com>
2025-02-08 15:17:37 -0500

    Add selected project table

--- sqlc/schema.sql
+++ sqlc/schema.sql
@@ -8,11 +8,17 @@ CREATE TABLE IF NOT EXISTS requests (
   id         INTEGER PRIMARY KEY,
   project_id INTEGER NOT NULL,
   name       TEXT    NOT NULL,
+  curl       TEXT    NOT NULL,
   method     TEXT    NOT NULL,
   url        TEXT    NOT NULL,
   headers    TEXT,
   body       TEXT,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   updated_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (project_id) REFERENCES projects(id)
+);
+
+CREATE TABLE IF NOT EXISTS selected_project (
+  project_id INTEGER NOT NULL,
   FOREIGN KEY (project_id) REFERENCES projects(id)
 );
