441d6ef2e5551e0310024297286b2695c5750804
austingw <senortinman@gmail.com>
2025-02-10 22:00:47 -0500

    Add list command

--- cmd/list.go
+++ cmd/list.go
@@ -1,18 +1,19 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/austingw/reqord/db"
 	"github.com/spf13/cobra"
 )
 
-// digCmd represents the dig command
-var digCmd = &cobra.Command{
-	Use:   "dig",
+// listCmd represents the list command
+var listCmd = &cobra.Command{
+	Use:   "list",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -21,20 +22,31 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("dig called")
+		ctx := context.Background()
+		queries, err := db.GetQueries()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		reqList, err := queries.ListProjectRequests(ctx, 1)
+		for _, r := range reqList {
+			// just print for now, we'll add bubble tea component later
+			fmt.Println(r)
+		}
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(digCmd)
+	rootCmd.AddCommand(listCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// digCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// listCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// digCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// listCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
