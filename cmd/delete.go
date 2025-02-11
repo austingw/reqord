8f3e9edff7d6bac34069fb059f1d893c2b5a0b97
austingw <senortinman@gmail.com>
2025-02-11 18:57:28 -0500

    Add delete command

--- cmd/delete.go
+++ cmd/delete.go
@@ -1,18 +1,19 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/austingw/reqord/db"
 	"github.com/spf13/cobra"
 )
 
-// scratchCmd represents the scratch command
-var scratchCmd = &cobra.Command{
-	Use:   "scratch",
+// deleteCmd represents the delete command
+var deleteCmd = &cobra.Command{
+	Use:   "delete",
 	Short: "A brief description of your command",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
@@ -21,20 +22,39 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("scratch called")
+		ctx := context.Background()
+		queries, err := db.GetQueries()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		request, err := queries.GetRequest(ctx, db.GetRequestParams{
+			ProjectID: 1,
+			Name:      args[0],
+		})
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		err = queries.DeleteRequest(ctx, request.ID)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("Succesfully delete request:", request.Name)
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(scratchCmd)
+	rootCmd.AddCommand(deleteCmd)
 
 	// Here you will define your flags and configuration settings.
 
 	// Cobra supports Persistent Flags which will work for this command
 	// and all subcommands, e.g.:
-	// scratchCmd.PersistentFlags().String("foo", "", "A help for foo")
+	// deleteCmd.PersistentFlags().String("foo", "", "A help for foo")
 
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
-	// scratchCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// deleteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
