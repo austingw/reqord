275c4321e0b6f2e074a23095e87d80d6faeb2aec
austingw <senortinman@gmail.com>
2025-02-15 21:54:38 -0500

    Add new flag for creating projects

--- cmd/project.go
+++ cmd/project.go
@@ -13,6 +13,8 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var New bool
+
 // projectCmd represents the project command
 var projectCmd = &cobra.Command{
 	Use:   "project",
@@ -34,20 +36,26 @@ to quickly create a Cobra application.`,
 		// check for existing project name
 		proj, err := queries.GetProject(ctx, args[0])
 
-		// if there's no project we make a new one and set it to current
-		if errors.Is(err, sql.ErrNoRows) {
-			newProj, err := queries.CreateProject(ctx, args[0])
-			if err != nil {
-				fmt.Println(err)
+		// If flag -n is used, we're making a new project :)
+		if New {
+			// Check for existing project
+			if proj.Name == args[0] {
+				fmt.Printf("Project with name %s already exists, if you would like to switch to it, remove the -n/--new flag", args[0])
+				return
+			} else if errors.Is(err, sql.ErrNoRows) {
+				newProj, err := queries.CreateProject(ctx, args[0])
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				err = queries.SetSelectedProject(ctx, newProj.ID)
+				if err != nil {
+					fmt.Println(err)
+					return
+				}
+				fmt.Printf("Successfully created and switched to new project: %s", newProj.Name)
 				return
 			}
-			err = queries.SetSelectedProject(ctx, newProj.ID)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fmt.Printf("Successfully created and switched to new project: %s", newProj.Name)
-			return
 		} else if err != nil {
 			fmt.Println(err)
 			return
@@ -75,4 +83,6 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// projectCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	projectCmd.Flags().BoolVarP(&New, "new", "n", false, "Create a new project and switch to it")
 }
